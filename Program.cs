0e090a6f70b866d6cfce864e138dd2c83159e3ef
dmitriy <boss.vede@mail.ru>
2022-08-18 18:19:21 +0700

    third exercise

--- Program.cs
+++ Program.cs
@@ -36,3 +36,17 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// int [] array = new int[8];
+// System.Console.WriteLine("Введите 8 чисел:");
+// for (int i = 0; i < 8; i++)
+// {
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     array[i] = number;
+// }
+// System.Console.Write("Полученный массив = [");
+// for (int i = 0; i < 8; i++)
+// {
+//     System.Console.Write($"{array[i]} ");
+// }
+// System.Console.Write("]");
